3c0c9722ed85ffcc6857d29bc6f7e903d45a34e3
Natrim <maly@kinet.cz>
2015-11-06 11:19:20 +0100

    force app reload if udpate ready on app start - this will hopefully fix chrome background page bug with updates  (the background app prevents automated updates)

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,14 +1,33 @@
 'use strict';
 
+chrome.runtime.onUpdateAvailable.addListener(function onUpdateAvailable() {
+  chrome.storage.local.set({
+    'updateReady': true
+  });
+});
+
 chrome.app.runtime.onLaunched.addListener(function onLaunched() {
-  chrome.app.window.create('index.html', {
-    id: 'fimfiction:main',
-    frame: 'none',
-    innerBounds: {
-      width: 1280,
-      height: 800,
-      minWidth: 375,
-      minHeight: 627
+  chrome.storage.local.get('updateReady', function loadGet(o) {
+    if (o.updateReady) {
+      chrome.runtime.reload();
+      return;
     }
+
+    chrome.app.window.create('index.html', {
+      id: 'fimfiction:main',
+      frame: 'none',
+      innerBounds: {
+        width: 1280,
+        height: 800,
+        minWidth: 375,
+        minHeight: 627
+      }
+    });
+  });
+});
+
+chrome.runtime.onInstalled.addListener(function onInstalled(e) {
+  chrome.storage.local.set({
+    'updateReady': false
   });
 });
