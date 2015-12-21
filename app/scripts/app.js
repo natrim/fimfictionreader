60478b3c7943d1aa368bbef2740407fb15028a5a
Natrim <maly@kinet.cz>
2015-12-21 10:29:24 +0100

    show msg if offline on app start

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,9 +33,15 @@ function createApp(AppConfig) {
       },
       '/offline': {
         component: {
-          template: '<p>OFFLINE</p>',
+          template: '<div class="ui modal active visible">' + '<div class="header">' + 'OFFLINE' + '</div>' + '<div class="content">' + AppConfig.translate('offlineDetail') + '<div class="description">' + '</div>' + '</div>' + '</div>',
           ready: function () {
             jQuery('#splash').dimmer('hide').remove();
+            var check = setInterval(function checkOnline() {
+              if (navigator.onLine) {
+                clearInterval(check);
+                router.go('/online');
+              }
+            }, 1000);
           }
         }
       }
