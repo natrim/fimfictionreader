f4f4d561163a007f3352fadddbdec3f1a1e4323b
Natrim <maly@kinet.cz>
2016-01-20 10:16:08 +0100

    clear offline check on offline descrtuctio

--- app/scripts/controller/offline.js
+++ app/scripts/controller/offline.js
@@ -5,13 +5,21 @@ function createOfflineController(router) {
   'use strict';
   var AppConfig = AppConfig || require('config');
   var l = AppConfig.translate;
+  var check;
   return {
     template: '<div class="ui modal active visible">' + '<div class="header">' + 'OFFLINE' + '</div>' + '<div class="content">' + '<div class="description">' + l('offlineDetail') + '</div>' + '</div>' + '</div>',
+    destroyed: function destroydOffline() {
+      if (check) {
+        clearInterval(check);
+        check = undefined;
+      }
+    },
     ready: function offlineReady() {
       jQuery('#splash').dimmer('hide').remove();
-      var check = setInterval(function checkOnline() {
+      check = setInterval(function checkOnline() {
         if (navigator.onLine) {
           clearInterval(check);
+          check = undefined;
           router.go('/online');
         }
       }, 1000);
