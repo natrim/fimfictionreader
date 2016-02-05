5ab71517ac004c18415b542d9beac954663f2bc1
Natrim <maly@kinet.cz>
2016-02-05 09:43:37 +0100

    radial on long click

--- app/scripts/vendor/radialmenu.js
+++ app/scripts/vendor/radialmenu.js
@@ -33,95 +33,85 @@
     var mouseY = 0;
     var radialMidX = 0;
     var radialMidY = 0;
-    //var currentRadialSelected = null;
-    var canMove = true;
+    var menuIsOpen = false;
 
-    function computeRadial(isRightMB, isClick) {
-      if (isRightMB) {
-        var deltax = mouseX - radialMidX;
-        var deltay = -mouseY + radialMidY;
-        var d = Math.sqrt(Math.pow(deltax, 2) + Math.pow(deltay, 2));
-        //$('.sector').css('border-bottom-color', 'royalblue');
-        $('.sector').removeClass('selected');
-        $('.sectorlabel').removeClass('selected');
-        if (d > 13) { // is 5 pixels a good amount to signify a drag evt
-          $('.vertex').removeClass('selvertex');
-          var r;
-          if (deltay >= 0) {
-            if (deltax >= 0) { // Q1
-              r = (3 * Math.PI / 2) - Math.atan(deltay / deltax);
-            } else { // Q2
-              r = (Math.PI / 2) + Math.atan(deltay / (-deltax));
-            }
-          } else {
-            if (deltax >= 0) { // Q4
-              r = (3 * Math.PI / 2) + Math.atan((-deltay) / (deltax));
-            } else { // Q3
-              r = (Math.PI / 2) - Math.atan((-deltay) / (-deltax));
-            }
-          }
-          var sang = (2 * Math.PI) / n;
-          var matchwhichsector = Math.floor((r + (sang / 2)) / sang);
-          if (matchwhichsector === n) { // because im dumb
-            matchwhichsector = 0;
-          }
-
-          if (isClick) {
-            if (!$('#sectorlabel' + matchwhichsector).find('a').hasClass('disabled')) {
-              $('#sectorlabel' + matchwhichsector).find('a').get(0).click();
-            }
-          } else {
-            $('#sector' + matchwhichsector).addClass('selected');
-            $('#sectorlabel' + matchwhichsector).addClass('selected');
+    function computeRadial(isClick) {
+      var deltax = mouseX - radialMidX;
+      var deltay = -mouseY + radialMidY;
+      var d = Math.sqrt(Math.pow(deltax, 2) + Math.pow(deltay, 2));
+      //$('.sector').css('border-bottom-color', 'royalblue');
+      $('.sector').removeClass('selected');
+      $('.sectorlabel').removeClass('selected');
+      if (d > 13) { // is 5 pixels a good amount to signify a drag evt
+        $('.vertex').removeClass('selvertex');
+        var r;
+        if (deltay >= 0) {
+          if (deltax >= 0) { // Q1
+            r = (3 * Math.PI / 2) - Math.atan(deltay / deltax);
+          } else { // Q2
+            r = (Math.PI / 2) + Math.atan(deltay / (-deltax));
           }
         } else {
-          $('.vertex').addClass('selvertex');
+          if (deltax >= 0) { // Q4
+            r = (3 * Math.PI / 2) + Math.atan((-deltay) / (deltax));
+          } else { // Q3
+            r = (Math.PI / 2) - Math.atan((-deltay) / (-deltax));
+          }
         }
+        var sang = (2 * Math.PI) / n;
+        var matchwhichsector = Math.floor((r + (sang / 2)) / sang);
+        if (matchwhichsector === n) { // because im dumb
+          matchwhichsector = 0;
+        }
+
+        if (isClick) {
+          if (!$('#sectorlabel' + matchwhichsector).find('a').hasClass('disabled')) {
+            $('#sectorlabel' + matchwhichsector).find('a').get(0).click();
+          }
+        } else {
+          $('#sector' + matchwhichsector).addClass('selected');
+          $('#sectorlabel' + matchwhichsector).addClass('selected');
+        }
+      } else {
+        $('.vertex').addClass('selvertex');
       }
     }
 
-
-    $(document).off('mousemove.radialMenu').on('mousemove.radialMenu', function menuMove(e) {
-      mouseX = e.pageX;
-      mouseY = e.pageY;
-      if (canMove) {
-        $('div.radialmenu').css('top', e.pageY).css('left', e.pageX - w - 2);
-        radialMidX = e.pageX;
-        radialMidY = e.pageY;
-      }
-
-      if (!canMove) {
-        var isRightMB;
-        e = e || window.event;
-        if ('which' in e) {
-          isRightMB = e.which === 3;
-        } else if ('button' in e) {
-          isRightMB = e.button === 2;
-        }
-
-        computeRadial(isRightMB, false);
+    $(document).off('touchcancel.radial').on('touchcancel.radial', function menuTC() {
+      if (clicking) {
+        clearTimeout(clicking);
+        clicking = null;
       }
     });
 
-    $(document).off('mouseup.radialMenu').on('mouseup.radialMenu', function menuMUp(e) {
+    $(document).off('mousemove.radial,touchmove.radial').on('mousemove.radial,touchmove.radial', function menuMove(e) {
+      if (clicking) {
+        clearTimeout(clicking);
+        clicking = null;
+      }
       mouseX = e.pageX;
       mouseY = e.pageY;
-      if (!canMove) {
-        var isRightMB;
-        e = e || window.event;
-        if ('which' in e) {
-          isRightMB = e.which === 3;
-        } else if ('button' in e) {
-          isRightMB = e.button === 2;
-        }
+      if (menuIsOpen) {
+        computeRadial(false);
+      }
+    });
 
-        computeRadial(isRightMB, true);
-        canMove = true;
+    var clicking = null;
+    $(document).off('mouseup.radial,touchend.radial').on('mouseup.radial,touchend.radial', function menuMUp(e) {
+      if (clicking) {
+        clearTimeout(clicking);
+        clicking = null;
+      }
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      if (menuIsOpen) {
+        computeRadial(true);
         $('.radialmenu').removeClass('paused');
+        menuIsOpen = false;
       }
     });
 
-    $(document).off('contextmenu.radialMenu').on('contextmenu.radialMenu', function menuContextTriggerPreventer(e) {
+    $(document).off('contextmenu.radial').on('contextmenu.radial', function menuContextTriggerPreventer(e) {
       if (!e.altKey) { // ALT Right Click => Standard Context Menu
         e.preventDefault();
         e.stopPropagation();
@@ -129,7 +119,27 @@
       }
     });
 
-    $(document).off('mousedown.radialMenu').on('mousedown.radialMenu', function menuMDown(e) {
+    function openMenu(e) {
+      $('div.radialmenu').css('top', e.pageY).css('left', e.pageX - w - 2);
+      $('.sector').removeClass('selected');
+      $('.sectorlabel').removeClass('selected');
+      $('.vertex').addClass('selvertex');
+
+      if (checkCallback) {
+        handleDisabledMenu();
+      }
+
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      $('div.radialmenu').css('top', e.pageY).css('left', e.pageX - w - 2);
+      radialMidX = e.pageX;
+      radialMidY = e.pageY;
+
+      $('.radialmenu').addClass('paused');
+      menuIsOpen = true;
+    }
+
+    $(document).off('mousedown.radial,touchstart.radial').on('mousedown.radial,touchstart.radial', function menuMDown(e) {
       var isRightMB;
       e = e || window.event;
       if ('which' in e) {
@@ -139,26 +149,13 @@
       }
 
       if (isRightMB && !e.altKey) {
-        if (canMove) {
-          canMove = false;
-          $('.sector').removeClass('selected');
-          $('.sectorlabel').removeClass('selected');
-          $('.vertex').addClass('selvertex');
-
-          if (checkCallback) {
-            handleDisabledMenu();
-          }
-
-          setTimeout(function () {
-            $('.radialmenu').addClass('paused');
-          }, 10);
-        } else {
-          canMove = true;
-          setTimeout(function () {
-            $('.radialmenu').removeClass('paused');
-          }, 10);
-        }
+        openMenu(e);
         return false;
+      } else {
+        clicking = setTimeout(function () {
+          clicking = null;
+          openMenu(e);
+        }, 500);
       }
     });
 
